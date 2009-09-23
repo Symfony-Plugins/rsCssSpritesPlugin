9c06b9bbcf3d489e0cf4b8658e92aab8d7868f14
caziel <caziel@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2009-09-23 14:38:58 -0500

    initial commit
    git-svn-id: http://svn.symfony-project.com/plugins/rsCssSpritesPlugin/trunk@22307 ee427ae8-e902-0410-961c-c3ed070cd9f9

---
+++ config/rsCssSpritesPluginConfiguration.class.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * rsCssSpritesPlugin configuration.
+ * 
+ * @package     rsCssSpritesPlugin
+ * @subpackage  config
+ * @author      Robert Schönthal caziel[at]gmx[dot]net
+ * @version     SVN: $Id: PluginConfiguration.class.php 12675 2008-11-06 08:07:42Z Kris.Wallsmith $
+ */
+class rsCssSpritesPluginConfiguration extends sfPluginConfiguration
+{
+  /**
+   * @see sfPluginConfiguration
+   */
+  public function initialize()
+  {
+  }
+}
---
+++ lib/task/rsCssSpritesTask.class.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * a simple task to generate css sprites for you
+ *
+ * @package     rsCssSpritesPlugin
+ * @subpackage  lib/task
+ * @author      Robert Schönthal caziel[at]gmx[dot]net
+ *
+ * call it with "symfony generate:sprites"
+ * first it should do nothing.
+ * see "http://csssprites.org/#usage" how to prepare your stylesheets
+ */
+class spritesTask extends sfBaseTask
+{
+  protected function configure()
+  {
+
+    $this->namespace        = 'generate';
+    $this->name             = 'sprites';
+    $this->briefDescription = 'generate css sprites';
+    $this->aliases          = array("generate-sprites");
+    $this->detailedDescription = <<<EOF
+The [sprites|INFO] task generates css sprites for you.
+Call it with:
+
+  [php symfony sprites|INFO]
+EOF;
+  }
+
+  protected function execute($arguments = array(), $options = array())
+  {
+    $params[] = "--root-dir-path ".sfConfig::get("sf_web_dir")."/css";
+    $params[] = "--sprite-png-ie6";
+    $params[] = "--document-root-dir-path ".sfConfig::get("sf_web_dir");
+
+    $chdir = "cd ".dirname(__FILE__)."/../vendor/smartsprites/";
+
+    $command = $chdir." && ./smartsprites.sh ".join(" ", $params);
+
+    $this->log(sfFilesystem::sh($command));
+  }
+}
